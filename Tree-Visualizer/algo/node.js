1f22895d63dcec94189432cdb4e333d65a2a777d
sayyedarib <sayyedaribhussain4321@gmail.com>
2023-08-28 00:26:03 +0530

    function for creating node
    Signed-off-by: sayyedarib <sayyedaribhussain4321@gmail.com>

--- Tree-Visualizer/algo/node.js
+++ Tree-Visualizer/algo/node.js
@@ -6,4 +6,19 @@ let arrayContainer;
 let start; 
 
 const xSpacing = 200;
-ySpacing = 100;
+const ySpacing = 100;
+const radius = 35;
+
+const Node = (value, index, depth, cx, cy) =>{
+    this.value = value;
+    this.index = index;
+    this.depth = depth;
+    this.radius = radius;
+    this.cx = cx;
+    this.cy = cy;
+    this.left = null;
+    this.right = null;
+    this.fill = regFill;
+    this.highlighted = false;
+} 
+
